bd6b5a4bf9d82377647fa9e0c1f4334e0119d5d9
Anurag Pandey <raar011@gmail.com>
2019-08-15 10:47:53 +0530

    Updates

---
+++ precache-manifest.60e488ae761c09705623362435193a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf30a80001c387318548b9f57b6b8c76",
+    "url": "/FX-Calculator/index.html"
+  },
+  {
+    "revision": "faef71e574c668796d9f",
+    "url": "/FX-Calculator/static/css/main.dcb2503c.chunk.css"
+  },
+  {
+    "revision": "a1f312fe14fdf1417dbc",
+    "url": "/FX-Calculator/static/js/2.efde1aae.chunk.js"
+  },
+  {
+    "revision": "faef71e574c668796d9f",
+    "url": "/FX-Calculator/static/js/main.4adf0c54.chunk.js"
+  },
+  {
+    "revision": "4451fcaeb5ba5de53ed9",
+    "url": "/FX-Calculator/static/js/runtime~main.e6f7785b.js"
+  }
+]);
